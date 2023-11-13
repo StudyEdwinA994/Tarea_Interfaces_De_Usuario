eaf350b0d9f0ca48b76e7e34d6f93ce3803e9d64
Edwin Alzate <EdwinAlzateA@>
2023-11-12 21:18:28 -0500

    Commit final del proyecto terminado.

--- index.js
+++ index.js
@@ -35,3 +35,18 @@ function bottonAgregarCarrito() {
   alert("Agregaste este producto al carrito");
 }
 
+/**/
+function informacionConocenos() {
+  var informacion = "Somos una tienda virtual encargada de vender todo tipo de productos para mascotas, nos aseguramos que tu amigo peludo sea feliz con nuestros productos.";
+  alert(informacion);
+}
+
+function informacionUbicaciones() {
+  var informacion = "Estamos ubicados en Medellín Carrera 50 con diagonal 38 local 202 (Al frente del éxito).";
+  alert(informacion);
+}
+
+function informacionContactanos() {
+  var informacion = "Puedes contactarnos en redes sociales facebook, instagram y twitter, encuentra los datos al final de la página.";
+  alert(informacion);
+}
